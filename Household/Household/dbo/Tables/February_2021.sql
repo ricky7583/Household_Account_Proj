438af66a41b193fb43dde51c2d70cad9a1d74897
ricky7583 <ricky7583@naver.com>
2021-09-14 19:01:26 -0400

    Update

--- Household/Household/dbo/Tables/February_2021.sql
+++ Household/Household/dbo/Tables/February_2021.sql
@@ -2,7 +2,7 @@
 (
 	Id INT NOT NULL PRIMARY KEY,
 	Date_of_Transaction DATE,
-	Amount DECIMAL(6,2), 
+	Amount DECIMAL(10,2), 
 	Details VARCHAR(30) NOT NULL,
 	Category VARCHAR(10),
 	Profit_or_Expense VARCHAR(8)
